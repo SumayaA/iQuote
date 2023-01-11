108216d06368307c89be89aa19ac602b7e674d55
sara <sara.ahumidi4@gmail.com>
2023-01-11 09:24:22 +0300

    Initial Commit

---
+++ iQuote/iQuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  iQuoteApp.swift
+//  iQuote
+//
+//  Created by Sara Alhumidi on 18/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct iQuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
